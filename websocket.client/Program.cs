32d698213a9524fe688b18c58e10cbf5c3480d14
mateus <amaral__@outlook.com>
2025-03-16 17:46:07 -0300

    Simples exemplo de Websocket nativo .net

---
+++ websocket.client/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Net.WebSockets;
+using System.Text;
+
+using var ws = new ClientWebSocket();
+await ws.ConnectAsync(new Uri("ws://localhost:5062/webteste"), CancellationToken.None);
+
+var buffer = new byte[1024];
+
+while (ws.State == WebSocketState.Open)
+{
+    var result = await ws.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+
+    if (result.MessageType == WebSocketMessageType.Close)
+    {
+        await ws.CloseAsync(WebSocketCloseStatus.NormalClosure, "Conexão encerrada", CancellationToken.None);
+        break;
+    }
+
+    string mensagem = Encoding.ASCII.GetString(buffer, 0, result.Count);
+    Console.WriteLine($"Mensagem do servidor: {mensagem}");
+
+    // Limpa o buffer
+    Array.Clear(buffer, 0, buffer.Length);
+}
--- websocket.server/Program.cs
+++ websocket.server/Program.cs
@@ -1,8 +1,13 @@
+using System.Net;
+using System.Net.WebSockets;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
@@ -10,6 +15,37 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-app.MapGet("/", () => "Hello World!");
 
-app.Run();
+var webSocketOptions = new WebSocketOptions
+{
+    KeepAliveInterval = TimeSpan.FromSeconds(120) 
+};
+app.UseWebSockets(webSocketOptions);
+
+app.Map("/webteste", async (HttpContext context) =>
+{
+    if (!context.WebSockets.IsWebSocketRequest)
+    {
+        context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+        return;
+    }
+
+    using var webSocket = await context.WebSockets.AcceptWebSocketAsync();
+    var buffer = new byte[1024];
+
+    try
+    {
+        while (webSocket.State == WebSocketState.Open)
+        {
+            var data = Encoding.ASCII.GetBytes($".Net -> {DateTime.Now}");
+            await webSocket.SendAsync(data, WebSocketMessageType.Text, true, CancellationToken.None);
+            await Task.Delay(1000);
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Erro no WebSocket: {ex.Message}");
+    }
+});
+
+await app.RunAsync();
